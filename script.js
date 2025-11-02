34d9dc6589ecc37bcc1ac55c13c5158596221b44
mvpconsultorialtda <118567695+mvpconsultorialtda@users.noreply.github.com>
2025-11-02 18:53:24 +0000

    update script

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-        // Adiciona interatividade simulada ao botão da carteira
-            const walletButton = document.querySelector('.add-to-wallet-btn');
-                
-                    if (walletButton) {
-                            walletButton.addEventListener('click', () => {
-                                        alert('A funcionalidade "Adicionar à Carteira" seria ativada aqui.');
-                                                });
-                                                    }
+    // Adiciona interatividade simulada ao botão de certificado
+    const certificateButton = document.querySelector('.certificate-btn');
+    
+    if (certificateButton) {
+        certificateButton.addEventListener('click', () => {
+            alert('A validação do certificado seria exibida aqui.');
+        });
+    }
 
-                                                        // Adiciona interatividade simulada ao botão de desbloqueio
-                                                            const unlockButton = document.querySelector('.notification-action');
-                                                                if (unlockButton) {
-                                                                        unlockButton.addEventListener('click', () => {
-                                                                                    alert('A tela para desbloquear o DNE físico seria aberta.');
-                                                                                            })
-                                                                                                }
-                                                                                                });
-})
+    // Adiciona interatividade simulada ao botão de desbloqueio
+    const unlockButton = document.querySelector('.notification-action');
+    if (unlockButton) {
+        unlockButton.addEventListener('click', () => {
+            alert('A tela para desbloquear o DNE físico seria aberta.');
+        })
+    }
+});
